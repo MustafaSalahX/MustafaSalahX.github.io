a6c0909f8c03e958eaaa5f76dab06ddbfe9a9d52
MustafaSalah <56977391+MustafaSalahX@users.noreply.github.com>
2025-02-25 20:42:38 +0200

    All Done

---
+++ DANIELS EXAM/js/script.js
@@ -0,0 +1,8 @@
+//#region MustafaSalah's Region For HeadText Typing
+var typed = new Typed("#animation-text", {
+  strings: [" Developer", " Designer ", "Larry Danils"],
+  typeSpeed: 30,
+  backSpeed: 20,
+  backDelay: 2000,
+  loop: true,
+});
