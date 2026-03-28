b0e9847ada862e6b53e39c02fd05085ab29833f9
MustafaSalahXS <mostafamahmoud5500@gmail.com>
2026-03-28 21:35:30 +0200

    Fix

--- js/script.js
+++
@@ -1,8 +0,0 @@
-//#region MustafaSalah's Region For HeadText Typing
-var typed = new Typed("#animation-text", {
-  strings: [" Developer", " Designer ", "Larry Danils"],
-  typeSpeed: 30,
-  backSpeed: 20,
-  backDelay: 2000,
-  loop: true,
-});
